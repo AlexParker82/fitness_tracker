8064c8cdc8735654923089d6f7b09a60b29b07a2
Alex Parker <alex.n.parker82@gmail.com>
2021-10-30 11:53:06 -0400

    continuing api routes

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,31 @@ const db = require("../models");
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({}).then((data) => {
         res.json(data);
+    }).catch((err) => {
+        res.json(err);
     })
-})
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+});
+
+router.post("/api/workouts/", (req, res) => {
+    db.Workout.create(req.body).then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+
+});
+
+router.post("/api/workouts/:id", (req, res) => {
+
+});
 
 module.exports = router;
 
